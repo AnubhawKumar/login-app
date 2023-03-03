b502e2925439b578229b4fd454b031617665b191
AnubhawKumar <92393354+AnubhawKumar@users.noreply.github.com>
2023-03-03 08:49:17 +0530

    feat: Wrapped the Logout button to NavLink

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import PageRoutes from './route/PageRoutes';
 function App() {
   return (
     <div className="App">
-      {/* <h1>Selenium App is in progress!! Are you excited aren't you?</h1> */}
       <PageRoutes/>
-      {/* <LoginPage /> */}
     </div>
   );
 }
--- src/components/NavigationPage.jsx
+++ src/components/NavigationPage.jsx
@@ -1,59 +1,46 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { RiLogoutCircleLine } from "react-icons/ri";
 import { localStorageKeys, removeLocalStorageItem } from "../shared/constants";
 const NavigationPage = () => {
-  const navigate = useNavigate();
   const handleLogOut = () => {
     removeLocalStorageItem(localStorageKeys.IS_LOGGED_IN);
-    navigate("/login");
   };
   return (
-    <>
-      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark ">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/dashboard">
-            Selenium App
-          </NavLink>
-          <button className="navbar-toggler" type="button">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarNavAltMarkup"
-            style={{ position: "relative" }}
-          >
-            <div className="navbar-nav">
-              <NavLink className="nav-link" to="/dashboard">
-                Home
-              </NavLink>
-              <NavLink className="nav-link" to="/about">
-                About
-              </NavLink>
-              <NavLink className="nav-link" to="/contact">
-                Contact
-              </NavLink>
-            </div>
-            <div
+    <nav className="navbar navbar-expand-lg  navbar-dark bg-dark ">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/dashboard">
+          Selenium App
+        </NavLink>
+        <button className="navbar-toggler" type="button">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse navbar-div"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav">
+            <NavLink className="nav-link" to="/dashboard">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+            <NavLink
+              to="/login"
+              className="nav-link logout-btn"
               onClick={() => handleLogOut()}
-              style={{
-                textDecoration: "none",
-                right: 0,
-                position: "absolute",
-                cursor: "pointer",
-                color: "rgba(255, 255, 255, 0.55)",
-              }}
             >
-              <RiLogoutCircleLine
-                className="mx-1"
-                style={{ marginBottom: "2" }}
-              />
-              <span>Logout</span>
-            </div>
+              <RiLogoutCircleLine className="mx-1 logout-icon" />
+              Logout
+            </NavLink>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
